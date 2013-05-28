90c526598a8c5b6a7c4f7c2f6cf3f194fe27c96b
Andy <andrew.farrell@phgroup.com>
2013-05-28 11:29:46 +0100

    Add options to include random punctuation/capitalisation in the generated pp.

--- public/js/chbs.js
+++ public/js/chbs.js
@@ -1,7 +1,9 @@
 (function( D ) {
     var another = D.getElementById("pp-another"),
         pp_out = D.getElementById("pp-phrase"),
-        pp_length = D.getElementById("pp-length-output");
+        pp_length = D.getElementById("pp-length-output"),
+        pp_punc = D.getElementById("pp-punc"),
+        pp_caps = D.getElementById("pp-caps");
 
     function selectText( element ) {
         // Select the text content of the given element.
@@ -20,6 +22,36 @@
         }
     }
 
+    function addPunctuation( words ) {
+        var ends = [
+                "!",
+                ",",
+                ".",
+                "?",
+                ":",
+                ";",
+                " -",
+                "..."
+            ];
+
+        return words.map(function( word ) {
+            return word +
+                ends[ ( Math.random() * ends.length ) | 0 ];
+        });
+    }
+
+    function addCapitals( words ) {
+        return words.map(function( word ) {
+            if ( Math.random() < 0.5 ) {
+                word = word.split("");
+                word[0] = word[0].toUpperCase();
+                word = word.join("");
+            }
+
+            return word;
+        });
+    }
+
     another.onclick = function() {
         var xhr = new XMLHttpRequest(),
             num_words = 4;
@@ -27,7 +59,17 @@
         xhr.open( "GET", "/chbs-gen?words=" + num_words, true );
 
         xhr.onload = function() {
-            var words = JSON.parse( this.responseText ).join(" ");
+            var words = JSON.parse( this.responseText );
+
+            if ( pp_punc.checked ) {
+                words = addPunctuation( words );
+            }
+
+            if ( pp_caps.checked ) {
+                words = addCapitals( words );
+            }
+
+            words = words.join(" ");
             pp_out.innerHTML = words;
             pp_length.innerHTML = words.length;
         };
