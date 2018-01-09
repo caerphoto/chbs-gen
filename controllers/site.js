cb9c21fd85be8fde1d883bc167f4d1043f979865
Andy Farrell <andrew.farrell@experian.com>
2018-01-09 17:29:35 +0000

    Change logging format; use new fonts.

--- controllers/site.js
+++ controllers/site.js
@@ -40,12 +40,12 @@ function log( words, ip, load ) {
 
     console.log(
         "\033[33m" + ip + "\033[0m",
-        now.getUTCHours() + ":" + ( mins < 10 ? "0" + mins : mins ),
+        (days[ now.getDay() - 1 ]) || now.getDay() - 1, now.getDate(),
         days[ now.getDay() - 1 ], now.getDate(),
         months[ now.getMonth() ],
         "[\033[32m", words.join(" "), "\033[0m]",
         words.length + " " + words.join(" ").length,
-        s );
+        s);
 }
 
 exports.index = function( req, res ) {
