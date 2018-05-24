4caae229b14a485defdb9acd328587b951b40a24
Andy Farrell <andrew.farrell@experian.com>
2018-05-24 09:33:28 +0100

    Fix indentation and quotes.

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-var express = require("express"),
-    app = express(),
+var express = require('express'),
+  app = express(),
 
-    site = require("./controllers/site");
+  site = require('./controllers/site');
 
-app.set( "views", __dirname + "/views" );
-app.set( "view engine", "ejs" );
-app.set( "trust proxy", true );
+app.set( 'views', __dirname + '/views' );
+app.set( 'view engine', 'ejs' );
+app.set( 'trust proxy', true );
 app.use( express.static(__dirname + '/public') );
 app.use( express.bodyParser() );
 
-app.get( "/chbs-gen", site.index );
+app.get( '/chbs-gen', site.index );
 
-if ( app.get("env") === "production" ) {
-    app.listen( 3006, "localhost" );
-    console.log("Listening to localhost on 3006");
+if ( app.get('env') === 'production' ) {
+  app.listen( 3006, 'localhost' );
+  console.log('Listening to localhost on 3006');
 } else {
-    app.listen(3006);
-    console.log("Listening on 3006");
+  app.listen(3006);
+  console.log('Listening on 3006');
 }
